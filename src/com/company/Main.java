354a8bf92da39549cc9c4943d2751c7f07a4887a
maxssst <starushko.max@gmail.com>
2022-10-31 04:01:18 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import arduino.Arduino;
+
+
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException, FileNotFoundException {
+
+        Arduino arduino = new Arduino("COM3", 9600);
+
+        boolean connected = arduino.openConnection();
+        System.out.println("Connected: " + connected + arduino.getSerialPort());
+
+        String data = "";
+        int count = 0;
+
+        while (arduino.openConnection()) {
+            for (int i = 0; i < 10; i++) {
+                if (count <= 10) {
+                    data += arduino.serialRead();
+                    System.out.println(data);
+                    count ++;
+                    Thread.sleep(2000);
+                }
+                try {
+                    BufferedWriter writer = new BufferedWriter(new FileWriter("outputData" + count + ".txt"));
+                    writer.write(data);
+                    writer.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Thread.sleep(2000);
+            }
+
+
+        }
+
+    }
+}
